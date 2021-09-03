6ece51b76a81a4d411f73a7451dbcca82cfcb82c
Muhammad.Junaid <Muhammad.Junaid@emaratech.ae>
2021-09-03 08:56:00 +0400

    Base project using STS

---
+++ src/main/java/com/terminius/test/TerminiusTestApplication.java
@@ -0,0 +1,13 @@
+package com.terminius.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminiusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminiusTestApplication.class, args);
+	}
+
+}
